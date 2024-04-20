978059f9ee43ee3a3d26d5458642123f854a71ad
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-20 20:44:54 +0200

    created rectangle and sliders

--- RectangleRGBSwiftUI/ContentView.swift
+++ RectangleRGBSwiftUI/ContentView.swift
@@ -22,3 +22,39 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct ColorRectangle: View {
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .frame(width: 300, height: 150)
+            .foregroundStyle(
+                Color(
+                    red: redValue / 255,
+                    green: greenValue / 255,
+                    blue: blueValue / 255
+                )
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius:20)
+                    .stroke(lineWidth: 4)
+            )
+    }
+}
+
+struct ColorSlider: View {
+    let tint: Color
+    @Binding var colorValue: Double
+    
+    var body: some View {
+        HStack(spacing: 10) {
+            Text(lround(colorValue).formatted())
+                .frame(width: 40)
+            Slider(value: $colorValue, in: 0...255, step: 1)
+                .tint(tint)
+        }
+    }
+}
