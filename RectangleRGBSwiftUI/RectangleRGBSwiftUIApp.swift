2a8a048be1617756bd613af8f9f3cb8c41a9b83f
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-20 19:48:52 +0200

    Initial Commit

---
+++ RectangleRGBSwiftUI/RectangleRGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleRGBSwiftUIApp.swift
+//  RectangleRGBSwiftUI
+//
+//  Created by Denis Denisov on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RectangleRGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
